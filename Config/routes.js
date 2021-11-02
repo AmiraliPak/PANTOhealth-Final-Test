6f6ebeff5b177480fe5fbeed5ef0a09302221f95
AmiraliPak <a4amirali@hotmail.com>
2021-11-02 21:58:32 +0330

    feat(cputemp): add POST API route and controller

--- Config/routes.js
+++ Config/routes.js
@@ -1,3 +1,7 @@
+const cpuTempsController = require("../Src/Controllers/CpuTempsController");
+
 module.exports = (app) => {
 
+    app.post("/cputemps", cpuTempsController.insert); // post value in influxDB
+    app.get("/cputemps/test", cpuTempsController.getTemp); // gets instant temperature
 }
