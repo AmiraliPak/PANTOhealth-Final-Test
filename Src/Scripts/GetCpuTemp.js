fc353f210acb1f200b39fa49480461dbdd66456d
AmiraliPak <a4amirali@hotmail.com>
2021-11-02 21:51:27 +0330

    feat(cputemp): add script to get cpu temperature

---
+++ Src/Scripts/GetCpuTemp.js
@@ -0,0 +1,30 @@
+const si = require('systeminformation');
+
+/**
+ * Gets current CPU temperature.
+ * @returns {number}
+ */
+const getCpuTemperature = async () => {
+    const data = await si.cpuTemperature();
+    if (data.main) { // main could be NaN
+        // console.log(`main temperature: ${data.main}`);
+        return data.main;
+    }
+    
+    const cpuTemps = data.cores;
+    // console.log(`core temperatures: ${cpuTemps}`);
+    let lenTemps = 0, sumTemps = 0;
+    for (temp of cpuTemps) {
+        if (temp) { // temp could be NaN
+            sumTemps += temp;
+            lenTemps++;
+        }
+    }
+    if (sumTemps == 0 || lenTemps == 0)
+        throw "CPU temperature unavailable. Be sure to have elevated privileges / run as administrator.";
+    const avgCpuTemp = sumTemps / lenTemps;
+    // console.log(`average core temperatures: ${avgCpuTemp}`);
+    return avgCpuTemp;
+};
+
+module.exports = { getCpuTemperature };
